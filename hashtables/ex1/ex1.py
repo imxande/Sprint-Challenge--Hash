5b047a3ff4152ef9a5f298892594e1df1ac56498
imxande <alerodriguezlarosa@gmail.com>
2020-05-08 12:33:29 -0400

    ex1 pass test

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,24 @@
 def get_indices_of_item_weights(weights, length, limit):
 
-    """
-    YOUR CODE HERE
-    """
+    # create a dictionary
+    items = dict()
 
+    # read the weight list, I need to loop
+    for i in range(length):
+        # diff place holder for the item I am looking for
+        diff = items.get(limit - weights[i])
+        
+        # if I need to find the sum then i can check if diff already exist
+        if diff != None:
+            # if so then return the (zero, one) form tuple
+            return (i, diff)
+        else:
+            # if diff does not exist then create key value pair relationship
+            items[weights[i]] = i
+
+    print(items)
+
+    # our function return none if there is not pair
     return None
+
+    
